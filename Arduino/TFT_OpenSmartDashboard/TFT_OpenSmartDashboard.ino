8b3d7a0ffc5845ab9cd6b2a21cf8c913c7bcb7bb
kletellier <gregory.letellier@gmail.com>
2020-11-21 09:23:00 +0100

    Add TFT OpenSmart arduino for Mega

--- Arduino/TFT_OpenSmartDashboard/TFT_OpenSmartDashboard.ino
+++ Arduino/TFT_OpenSmartDashboard/TFT_OpenSmartDashboard.ino
@@ -16,11 +16,6 @@
 #define YM 9   // can be a digital pin
 #define XP 8   // can be a digital pin
 
-#define TS_MINX 150
-#define TS_MINY 120
-#define TS_MAXX 920
-#define TS_MAXY 940
-
 #define LCD_CS A3
 #define LCD_CD A2
 #define LCD_WR A1
@@ -38,9 +33,7 @@
 
 #define ORANGE_TRIGGER  192
 #define RED_TRIGGER  256
-
-#define MINPRESSURE 100
-#define MAXPRESSURE 1000
+ 
 
 typedef enum {
   RPM,
@@ -64,31 +57,24 @@ typedef enum {
   FUEL_AUTONOMY
 } MessageType;
 
-typedef struct {
-   String Rpm;
-   int Percent;
-   String Speed;
-   String Gear;
-   } Display_1;
-
-typedef struct {
-  String Fuel;
-  int Percent;
-  String Lap;
-} Display_2;
-
-typedef struct {
-  long BestTime;
-  long LastTime;
-  String Lap;
-  String Position;
-} Display_3;
-
 
 typedef struct {
   String Command;
   String Value;
 } Command;
+
+typedef struct {
+    int Percent;
+    String Speed;
+    String Gear;
+    long BestTime;
+    long LastTime;
+    String Fuel;
+    String Lap;
+    String Rpm;
+    String Position;
+   } Display;
+ 
 
 typedef struct {
   int Percent;
@@ -98,6 +84,8 @@ typedef struct {
   long LastTime;
   String Fuel;
   String Lap;
+   String Rpm;
+   String Position;
 } LastDisplay;
  
   
@@ -111,22 +99,18 @@ byte rotation = 3;
 long diffLess;
 bool bForceDisplay = false;
 
-Display_1 stDisplay1 = {"0",0,"0","N"};
-Display_2 stDisplay2 = {"0",0,""};
-Display_3 stDisplay3 = {0,0,"0","_"};
  
-LastDisplay stLast = {0,"0","N",0,0,"0",""};
+Display stDisplay = {0,"0","N",0,0,"0","","",""}; 
+LastDisplay stLast = {0,"0","N",0,0,"0","","",""};
  
 
-OPENSMART_kbv tft;
-//Adafruit_TFTLCD tft(LCD_CS, LCD_CD, LCD_WR, LCD_RD, LCD_RESET); 
-TouchScreen ts = TouchScreen(XP, YP, XM, YM, 300);
+OPENSMART_kbv tft; 
 
 void setup(void) {
   Serial.begin(115200);   
   tft.reset();  
   uint16_t ID = tft.readID();
-  if (ID == 0xD3D3) ID = 0x9481;  
+  if (ID == 0xD3D3) ID = 0x9481; // Fix for openSmart TFT 
   tft.begin(ID);
   ResetDisplay(); 
   pinMode(13, OUTPUT);
@@ -174,27 +158,27 @@ void ParseCommand(String pStr)
   switch (cmd.Command.toInt())
   {
     case RPM:
-      stDisplay1.Rpm = iValue;
+      stDisplay.Rpm = iValue;
       break;
     case RPM_TICK:
-      stDisplay1.Percent = iValue.toInt();
+      stDisplay.Percent = iValue.toInt();
       break;
     case SPEED:
-      stDisplay1.Speed = iValue;
+      stDisplay.Speed = iValue;
       break;
     case GEAR:
-      stDisplay1.Gear = iValue;
+      stDisplay.Gear = iValue;
       break;  
     case FUEL:
-      stDisplay2.Fuel = iValue;      
+      stDisplay.Fuel = iValue;      
     case FUEL_TICK:
-      stDisplay2.Percent = iValue.toInt();
+      stDisplay.Percent = iValue.toInt();
       break; 
     case CURRENT_POSITION:
-      stDisplay3.Position = iValue;
+      stDisplay.Position = iValue;
       break; 
     case LAP:
-      stDisplay3.Lap = iValue;
+      stDisplay.Lap = iValue;
       if(iLap!=iValue.toInt())
       {          
         iLap = iValue.toInt(); 
@@ -203,8 +187,8 @@ void ParseCommand(String pStr)
     case TIME:
       char *timeArray[2];
       SplitToArray(iValue,timeArray);
-      stDisplay3.BestTime = StringToLong(timeArray[0]);
-      stDisplay3.LastTime = StringToLong(timeArray[1]);
+      stDisplay.BestTime = StringToLong(timeArray[0]);
+      stDisplay.LastTime = StringToLong(timeArray[1]);
       break; 
         
     default:
@@ -241,12 +225,12 @@ void RefreshDisplay()
 void DisplayMode1()
 {
   // Display Speed,Gear,Rpm , Fuel , Fuel remaining estimated laps, Best Time and LastTime
-  if(bForceDisplay || stLast.Speed!=stDisplay1.Speed)
+  if(bForceDisplay || stLast.Speed!=stDisplay.Speed)
   {
     char last[10];
     char actual[10];
 
-    padLeft(stDisplay1.Speed," ",3).toCharArray(actual,sizeof(actual));
+    padLeft(stDisplay.Speed," ",3).toCharArray(actual,sizeof(actual));
     padLeft(stLast.Speed," ",3).toCharArray(last,sizeof(last));
 
     for (int i = 0; i < 3; i++)
@@ -260,13 +244,13 @@ void DisplayMode1()
           tft.drawChar(iOffset,0, act_char, WHITE, BLACK, 9);
         }
     }  
-    stLast.Speed = stDisplay1.Speed;
+    stLast.Speed = stDisplay.Speed;
   }
 
-  if(bForceDisplay || stLast.Percent!=stDisplay1.Percent)
+  if(bForceDisplay || stLast.Percent!=stDisplay.Percent)
   {      
-    int iPix = (int) stDisplay1.Percent * 3.2;       
-      if(stLast.Percent>stDisplay1.Percent)
+    int iPix = (int) stDisplay.Percent * 3.2;       
+      if(stLast.Percent>stDisplay.Percent)
       {
         // draw black offset
         tft.fillRect(iPix,81,iLastPix-iPix,38,BLACK);
@@ -293,56 +277,56 @@ void DisplayMode1()
     tft.fillRect(0,81,iGreen,38,GREEN);     
     
     iLastPix = iPix;
-    stLast.Percent = stDisplay1.Percent;      
+    stLast.Percent = stDisplay.Percent;      
   }
 
-  if(bForceDisplay || stLast.Gear!=stDisplay1.Gear)
+  if(bForceDisplay || stLast.Gear!=stDisplay.Gear)
   {          
     char cSpeed[2];
-    stDisplay1.Gear.toCharArray(cSpeed,sizeof(cSpeed));
+    stDisplay.Gear.toCharArray(cSpeed,sizeof(cSpeed));
     tft.drawChar(260,0, cSpeed[0], GREEN, BLACK, 10);         
-    stLast.Gear = stDisplay1.Gear;
+    stLast.Gear = stDisplay.Gear;
   }     
 
-  if(bForceDisplay || stLast.BestTime!=stDisplay3.BestTime)
+  if(bForceDisplay || stLast.BestTime!=stDisplay.BestTime)
   {        
     tft.setTextColor(WHITE,BLACK);
     tft.setTextSize(2);
     tft.setCursor(15,200);
     char pStr[10];
-    FormatTime(pStr,stDisplay3.BestTime) ;
+    FormatTime(pStr,stDisplay.BestTime) ;
     tft.println(pStr);
-    stLast.BestTime = stDisplay3.BestTime;
+    stLast.BestTime = stDisplay.BestTime;
   }
         
-  if(bForceDisplay || stLast.LastTime!=stDisplay3.LastTime)
+  if(bForceDisplay || stLast.LastTime!=stDisplay.LastTime)
   {       
     tft.setTextColor(WHITE,BLACK);
     tft.setTextSize(2);
     char pStr2[10];
     tft.setCursor(185,200);
-    FormatTime(pStr2,stDisplay3.LastTime) ;
+    FormatTime(pStr2,stDisplay.LastTime) ;
     tft.println(pStr2);
-    stLast.LastTime = stDisplay3.LastTime;
+    stLast.LastTime = stDisplay.LastTime;
   } 
 
   if(diffLess>1000)
   {
-      if(bForceDisplay || stLast.Fuel!=stDisplay2.Fuel)
+      if(bForceDisplay || stLast.Fuel!=stDisplay.Fuel)
       {         
         tft.setTextSize(3);
         tft.setCursor(10,135);
         tft.setTextColor(WHITE,BLACK);
-        tft.println(padLeft(stDisplay2.Fuel," ",6));
-        stLast.Fuel = stDisplay2.Fuel;
+        tft.println(padLeft(stDisplay.Fuel," ",6));
+        stLast.Fuel = stDisplay.Fuel;
       }
 
-      if(bForceDisplay || stLast.Lap!=stDisplay2.Lap)
+      if(bForceDisplay || stLast.Lap!=stDisplay.Lap)
       {
         tft.setTextColor(WHITE,BLACK);
         tft.setCursor(175,135);
-        tft.println(padLeft(stDisplay2.Lap," ",6));
-        stLast.Lap = stDisplay2.Lap;
+        tft.println(padLeft(stDisplay.Lap," ",6));
+        stLast.Lap = stDisplay.Lap;
       }
     
     
